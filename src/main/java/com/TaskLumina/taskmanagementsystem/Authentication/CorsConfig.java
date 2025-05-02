d24a41711d26091df57ea2fe2fd4ffe9df4e4031
AASani29 <alfey2001sani@gmail.com>
2025-05-03 00:05:42 +0600

    CORS rules modified

--- src/main/java/com/TaskLumina/taskmanagementsystem/Authentication/CorsConfig.java
+++ src/main/java/com/TaskLumina/taskmanagementsystem/Authentication/CorsConfig.java
@@ -1,6 +1,5 @@
 package com.TaskLumina.taskmanagementsystem.Authentication;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -15,9 +14,9 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedOrigins("*");
+                        .allowedOrigins("*") // Allow all origins
+                        .allowedMethods("GET", "POST", "PUT", "DELETE") // Allowed methods
+                        .allowedHeaders("*"); // Allow all headers
             }
         };
     }
